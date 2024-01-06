28846fdbb1eb833645639b7156d9cb5ead5241dc
Dipak Deshmukh <dipak.deshmukh1911@gmail.com>
2024-01-06 16:10:23 +0530

    changes

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
-/**
- * <div  id="parent">
- *      <div id="child">
- *        <h1>i'am h1 tag </h1>
- *     </div>
- * </div> 
- */
- const parent = React.createElement(
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
     "div",
     { id : "parent"},
    [ React.createElement(
     "div",
         {id : "child1"},[
-        React.createElement("h1",{},"I'am an h1 tag"),
-        React.createElement("h2",{},"I'am an h2 tag")
+        React.createElement("h1",{},"this is a namste react 🚀"),
+        React.createElement("h2",{},"by Dipak Deshmukh")
     ]), React.createElement(
         "div",
             {id : "child2"},[
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # namste react 🚀
+
+# parcel
+- dev build 
+- local server
+- HMR - hot module replacement
+- file watching algoritham - written int c++
+- caching - faster builds
+- image optimization
+- minifiaction
+- bundling 
+- compress
+- consitent hashing 
+- diffrential bundling - support older browser
+- diagnostic 
+- error handling 
+- https
+- tree shaking - remove unused code for you 
+- diffrent dev and production bundles
